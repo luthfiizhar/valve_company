492c03ff0f2f3089520e7a25d8f2bedacde3d05f
Luthfi Izhariman <59906471+luthfiizhar@users.noreply.github.com>
2025-02-12 22:39:10 +0700

    12/02/2024 22:39

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // output: "standalone",
   // basePath: "/stock-of-valve",
   // reactStrictMode: true,
   // distDir: "dist",

9bfdb2f3f321e5f837878bc368231db0d2149fa8
Luthfi Izhariman <59906471+luthfiizhar@users.noreply.github.com>
2025-04-12 09:59:41 +0700

    Update ProductComponent.tsx

--- app/components/ourProductsSection/ProductComponent.tsx
+++ app/components/ourProductsSection/ProductComponent.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { BsBoxArrowUpRight } from "react-icons/bs";
 import Link from "next/link";
 import Image from "next/image";
 

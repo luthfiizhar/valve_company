fca06a8138373d8bb8cd8e96b28f4a7dc12bb75e
Luthfi Izhariman <59906471+luthfiizhar@users.noreply.github.com>
2025-04-14 22:23:57 +0700

    Update ContactInfoComponent.tsx

--- app/contact/ContactInfoComponent.tsx
+++ app/contact/ContactInfoComponent.tsx
@@ -7,8 +7,10 @@ interface ContactInfoComponentProps {
 
 const ContactInfoComponent = ({ text, icon }: ContactInfoComponentProps) => {
   return (
-    <div className="flex flex-row gap-[8px] items-center  lg:w-auto lg:items-start">
-      {icon}
+    <div className="flex flex-row gap-[8px] items-center lg:w-auto lg:items-start">
+      <div>
+        {icon}
+      </div>
       <span className="text-[16px] text-primary font-medium lg:max-w-[222px]">
         {text}
       </span>

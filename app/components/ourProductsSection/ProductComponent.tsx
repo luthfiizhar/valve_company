b9ede62bf86506057c77f39204a9a371f6239465
Luthfi Izhariman <59906471+luthfiizhar@users.noreply.github.com>
2025-04-26 21:48:18 +0700

    Update ProductComponent.tsx

--- app/components/ourProductsSection/ProductComponent.tsx
+++ app/components/ourProductsSection/ProductComponent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+// import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
